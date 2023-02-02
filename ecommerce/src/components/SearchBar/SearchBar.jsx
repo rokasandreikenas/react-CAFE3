1121850d060d79030b8f7aff5ce6d40489f8f6e1
Rokas Andreikenas <shyptt@gmail.com>
2023-02-02 20:02:25 +0200

    Add SearchBar

--- ecommerce/src/components/SearchBar/SearchBar.jsx
+++ ecommerce/src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,33 @@
+import styled from "styled-components";
+import { searchBgColor } from "../../consts/colors";
+import { AiOutlineSearch } from "react-icons/ai";
+
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <InputWrapper>
+      <AiOutlineSearch />
+      <Input placeholder="Search" />
+    </InputWrapper>
+  );
 };
 
 export default SearchBar;
+
+const InputWrapper = styled.div`
+  position: relative;
+
+  svg {
+    font-size: 22px;
+    position: absolute;
+    left: 12px;
+    top: 5px;
+  }
+`;
+
+const Input = styled.input`
+  background-color: ${searchBgColor};
+  border: none;
+  outline: none;
+  padding: 8px 40px;
+  border-radius: 4px;
+`;
